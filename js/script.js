6a8f469f6b39cc44c1d51ad0efff58dfb128ade5
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2023-05-31 05:43:13 +0300

    fixed accordeon and tabs, added some function for search form for button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 new Accordion('.accordion-container');
 const $burger = document.getElementById("burger"),
   $search = document.getElementById("search-icon"),
+  $searchBtn = document.getElementById("header__close-svg"),
   $searchForm = document.querySelector(".header__wrap-input"),
   $burgerMenu = document.querySelector(".header"),
-  $stepsLink = document.querySelectorAll(".ul__steps-link"),
-  $stepsImg = document.querySelectorAll(".tab-img")
+  $stepsLink = document.querySelectorAll(".ul__steps-btn"),
+  $stepsInfo = document.querySelectorAll(".tab-content")
+
+const swiper = new Swiper('.swiper-container', {
+  // Цикличность
+  loop: true,
+  // Пагинация
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  a11y: {
+    paginationBulletMessage: 'Тут название слайда {{index}}',
+  }
+});
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -21,27 +35,27 @@ document.addEventListener("DOMContentLoaded", function () {
   })
 })
 
-const swiper = new Swiper('.swiper-container', {
-  // Цикличность
-  loop: true,
-  // Пагинация
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  a11y: {
-    paginationBulletMessage: 'Тут название слайда {{index}}',
-  }
-});
-
-for (let link of $stepsLink) {
-  link.addEventListener('click', function() {
-
-    for (let element of $stepsImg) {
-      element.classList.add('hidden')
-    }
-    
-    const content = document.querySelector('#' + link.dataset.tab);
-    content.classList.remove('hidden')
+document.addEventListener("DOMContentLoaded", function () {
+  $searchBtn.addEventListener("click", function () {
+    $searchForm.classList.remove("open")
   })
-}
+})
+
+$stepsLink.forEach(function (tabsBtn) {
+  tabsBtn.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    $stepsLink.forEach(function (btn) {
+      btn.classList.remove('ul__steps-btn--active')
+    });
+    e.currentTarget.classList.add('ul__steps-btn--active');
+
+    $stepsInfo.forEach(function (tab) {
+      tab.classList.remove('tab-content--active')
+    });
+
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(function (tab) {
+      tab.classList.add('tab-content--active');
+    });
+  });
+});
