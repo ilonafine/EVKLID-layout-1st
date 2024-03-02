29c53c182437dc10164b4b6b421b4f02add7b5d4
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2024-03-02 18:28:12 +0200

    added button for auto scroll page to top

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-new Accordion('.accordion-container');
+new Accordion('.accordion-container')
 const $burger = document.getElementById("burger"),
   $search = document.getElementById("search-icon"),
   $searchBtn = document.getElementById("header__close-svg"),
   $searchForm = document.querySelector(".header__wrap-input"),
   $burgerMenu = document.querySelector(".header"),
   $stepsLink = document.querySelectorAll(".ul__steps-btn"),
-  $stepsInfo = document.querySelectorAll(".tab-content")
+  $stepsInfo = document.querySelectorAll(".tab-content"),
+  $scrollBtn = document.getElementById('scroll-to-top')
 
+//swiper
 const swiper = new Swiper('.swiper-container', {
   // Цикличность
   loop: true,
@@ -18,45 +20,57 @@ const swiper = new Swiper('.swiper-container', {
   a11y: {
     paginationBulletMessage: 'Тут название слайда {{index}}',
   }
-});
+})
 
+//tabs
+$stepsLink.forEach(function (tabsBtn) {
+  tabsBtn.addEventListener('click', function (e) {
+    const path = e.currentTarget.dataset.path
 
+    $stepsLink.forEach(function (btn) {
+      btn.classList.remove('ul__steps-btn--active')
+    })
+    e.currentTarget.classList.add('ul__steps-btn--active')
+
+    $stepsInfo.forEach(function (tab) {
+      tab.classList.remove('tab-content--active')
+    })
+
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(function (tab) {
+      tab.classList.add('tab-content--active')
+    })
+  })
+})
+
+//burger and search
 document.addEventListener("DOMContentLoaded", function () {
+  //burger
   $burger.addEventListener("click", function () {
     $searchForm.classList.remove("open")
     $burgerMenu.classList.toggle("open")
   })
-})
-
-document.addEventListener("DOMContentLoaded", function () {
+  //search
   $search.addEventListener("click", function () {
     $burgerMenu.classList.remove("open")
     $searchForm.classList.toggle("open")
   })
-})
-
-document.addEventListener("DOMContentLoaded", function () {
   $searchBtn.addEventListener("click", function () {
     $searchForm.classList.remove("open")
   })
 })
 
-$stepsLink.forEach(function (tabsBtn) {
-  tabsBtn.addEventListener('click', function (e) {
-    const path = e.currentTarget.dataset.path;
+//auto scroll by button
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    $scrollBtn.classList.add('show')
+  } else {
+    $scrollBtn.classList.remove('show')
+  }
+})
 
-    $stepsLink.forEach(function (btn) {
-      btn.classList.remove('ul__steps-btn--active')
-    });
-    e.currentTarget.classList.add('ul__steps-btn--active');
-
-    $stepsInfo.forEach(function (tab) {
-      tab.classList.remove('tab-content--active')
-    });
-
-    document.querySelectorAll(`[data-target="${path}"]`).forEach(function (tab) {
-      tab.classList.add('tab-content--active');
-    });
-  });
-});
-
+$scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
